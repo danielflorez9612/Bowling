b3a54bc711c39378b2d5d14f845b3c9b50606384
Daniel Florez <daniel.florez.contractor@gdevtools.com>
2019-10-02 10:43:04 -0500

    default implementation for LineParser::parse

--- src/main/java/bowling/business/parser/ConsoleLineParser.java
+++ src/main/java/bowling/business/parser/ConsoleLineParser.java
@@ -8,24 +8,6 @@ public class ConsoleLineParser implements LineParser {
     private Integer pins;
 
     @Override
-    public ValidationError parse(String line) {
-        String[] frameParts = line.split(" ");
-        if (frameParts.length != 2) {
-            return new ValidationError(ValidationErrorCatalog.INVALID_FORMAT);
-        }
-        this.playerName = frameParts[0];
-        try {
-            this.pins = Integer.valueOf(frameParts[1]);
-            if (this.pins > 10 || this.pins < 0) {
-                return new ValidationError(ValidationErrorCatalog.INVALID_THROW);
-            }
-        } catch (NumberFormatException ignored) {
-            return new ValidationError(ValidationErrorCatalog.INVALID_FORMAT);
-        }
-        return null;
-    }
-
-    @Override
     public void onError(ValidationError err) {
         System.out.println(err.getError());
     }
--- src/main/java/bowling/business/parser/LineParser.java
+++ src/main/java/bowling/business/parser/LineParser.java
@@ -1,8 +1,27 @@
 package bowling.business.parser;
 
 public interface LineParser {
-    ValidationError parse(String line);
+    default ValidationError parse(String line) {
+        String[] frameParts = line.split(" ");
+        if (frameParts.length != 2) {
+            return new ValidationError(ValidationErrorCatalog.INVALID_FORMAT);
+        }
+        setPlayerName(frameParts[0]);
+        try {
+            setPins(Integer.valueOf(frameParts[1]));
+            if (getPins() > 10 || getPins() < 0) {
+                return new ValidationError(ValidationErrorCatalog.INVALID_THROW);
+            }
+        } catch (NumberFormatException ignored) {
+            return new ValidationError(ValidationErrorCatalog.INVALID_FORMAT);
+        }
+        return null;
+    }
     String getPlayerName();
+
+    void setPlayerName(String pN);
     Integer getPins();
+
+    void setPins(Integer pins);
     void onError(ValidationError err);
 }
