ab5b253b00ac3accf402e1cc25a5d0355c8087fa
Daniel Florez <daniel.florez.contractor@gdevtools.com>
2019-10-02 15:30:34 -0500

    chore testing config

---
+++ src/test/java/bowling/throwmarker/TenPinThrowMarkerTest.java
@@ -0,0 +1,14 @@
+package bowling.throwmarker;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TenPinThrowMarkerTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
